afccb79f4a43031568182e9c24f4776d7634291d
S3lfik <eduard.korogodov@gmail.com>
2024-08-15 12:13:30 +0300

    SB-19: add tags to the notes

--- note.py
+++ note.py
@@ -2,9 +2,10 @@ class Note:
     def __init__(self, title: str = "", body: str = ""):
         self.__title = title
         self.__body = body
+        self.__tags = []
 
     def __str__(self):
-        return f"{self.__title}: {self.__body}"
+        return f"Title: {self.__title}, Body: {self.__body}, Tags: {",".join(self.__tags)}"
 
     @property
     def title(self) -> str:
@@ -21,3 +22,11 @@ class Note:
     @body.setter
     def body(self, new_body: str):
         self.__body = new_body
+
+    @property
+    def tags(self) -> list:
+        return self.__tags
+
+    @tags.setter
+    def tags(self, new_tags: list):
+        self.__tags = new_tags
