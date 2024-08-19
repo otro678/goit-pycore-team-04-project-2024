81a893df20f80292eb866500aa6d6ca97fd968e3
Serhii K <serhii.kozachenko.92@gmail.com>
2024-08-19 15:37:41 +0300

    Fixed date presentation (removed time from birthday)

--- views/AddressBookView.py
+++ views/AddressBookView.py
@@ -4,6 +4,7 @@ from field import Field
 from record import Record, ADDRESS_BOOK_FIELDS
 from views.TableView import TableView
 from views.View import OutputData
+from datetime import datetime
 
 
 class AddressBookView(TableView):
@@ -26,6 +27,6 @@ class AddressBookView(TableView):
             self.escape(record.name.value if isinstance(record.name, Field) else ''),
             self.escape(", ".join(phone.value if isinstance(phone, Field) else '' for phone in record.phones)),
             self.escape(record.address.value if isinstance(record.address, Field) else ''),
-            self.escape(str(record.birthday.value if isinstance(record.birthday, Field) else '')),
+            self.escape(str(record.birthday.value.strftime("%d.%m.%Y") if isinstance(record.birthday, Field) else '')),
             self.escape(record.email.value if isinstance(record.email, Field) else ''),
         ]
